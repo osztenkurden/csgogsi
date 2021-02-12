af3eff62f5189d1ed5eecdc3fc0019ac32aefdbd
Hubert Walczak <hubertwalczak8@gmail.com>
2021-02-12 19:24:02 +0100

    Fixed config

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,32 +2,30 @@ import CSGOGSI from '../tsc';
 
 import * as PLANTING from './data/planting';
 
-test("creates CSGOGSI object", () => {
-    const GSI = new CSGOGSI();
-    expect(GSI).toBeDefined();
+test('creates CSGOGSI object', () => {
+	const GSI = new CSGOGSI();
+	expect(GSI).toBeDefined();
 });
 
-test("throws on bad data", () => {
-    const dummyData = {
-        allplayers: {},
-        map:{},
-        phase_countdowns:{}
-    } as any;
-    const GSI = new CSGOGSI();
-    expect(() => GSI.digest(dummyData)).toThrow();
+test('throws on bad data', () => {
+	const dummyData = {
+		allplayers: {},
+		map: {},
+		phase_countdowns: {}
+	} as any;
+	const GSI = new CSGOGSI();
+	expect(() => GSI.digest(dummyData)).toThrow();
 });
 
-test("calls plant start listeners", () => {
-    const onPlant = jest.fn(() => {});
+test('calls plant start listeners', () => {
+	const onPlant = jest.fn(() => {});
 
-    const GSI = new CSGOGSI();
+	const GSI = new CSGOGSI();
 
-    GSI.on("bombPlantStart", onPlant);
+	GSI.on('bombPlantStart', onPlant);
 
-    GSI.digest(PLANTING.start);
-    GSI.digest(PLANTING.end);
+	GSI.digest(PLANTING.start);
+	GSI.digest(PLANTING.end);
 
-    expect(onPlant.mock.calls.length).toBe(1);
-
-
+	expect(onPlant.mock.calls.length).toBe(1);
 });
