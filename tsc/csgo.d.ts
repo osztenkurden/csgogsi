3e9f2881e889979f36555703a400ffeeb62cc97f
osztenkurden <hubertwalczak8@gmail.com>
2026-09-15 13:22:23 +0200

    deps: update dependencies

--- tsc/csgo.d.ts
+++ tsc/csgo.d.ts
@@ -2,15 +2,7 @@ export type Side = 'CT' | 'T';
 export type RoundOutcome = 'ct_win_elimination' | 't_win_elimination' | 'ct_win_time' | 'ct_win_defuse' | 't_win_bomb';
 
 export type WeaponType =
-	| 'Knife'
-	| 'Pistol'
-	| 'Grenade'
-	| 'Rifle'
-	| 'SniperRifle'
-	| 'C4'
-	| 'Submachine Gun'
-	| 'Shotgun'
-	| 'Machine Gun';
+	'Knife' | 'Pistol' | 'Grenade' | 'Rifle' | 'SniperRifle' | 'C4' | 'Submachine Gun' | 'Shotgun' | 'Machine Gun';
 
 export interface WeaponRaw {
 	name: string;
